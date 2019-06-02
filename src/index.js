95a591d83daa8763f51323dddf93679dcf4faf79
Robert Worman <tr3ppy@gmail.com>
2019-06-02 16:24:47 +1000

    Add concept

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-export default function nothing() {
-  return;
+export default function iifeThunk() {
+  return {
+    generateBundle(o, x) {
+      if (o.format === 'iife') {
+        for (const bundle of Object.keys(x)) {
+          const start = `var ${o.name} = `
+          const end = /\);\n$/
+          x[bundle].code = x[bundle].code
+            .replace(start, start + 'function () { return ')
+            .replace(end, ')};\n')
+        }
+      }
+    },
+  }
 }
